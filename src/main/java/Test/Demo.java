63ec36dd3e4a14a40bc61295857e00ad06769da3
RKENDRE <Ranjeetvelocity13@gmail.com>
2022-12-28 20:28:17 +0530

    First commit

---
+++ src/main/java/Test/Demo.java
@@ -0,0 +1,25 @@
+package Test;
+
+import junit.framework.TestCase;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+}
